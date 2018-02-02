c57c1678eb999495877c6bc0a09cbfe6209ac726
rob mcgrath <117702939@umail.ucc.ie>
2018-02-02 13:03:24 +0000

    css

---
+++ interests/ebusiness/cost_calc.js
@@ -0,0 +1,33 @@
+/* global $ */
+
+function calcSub(){
+    
+    var argSubTotal;
+    
+    if(document.getElementById('salesforce').checked){
+        argSubTotal = 100;
+    }
+    else {
+        argSubTotal = 300;
+    }
+    
+    display(argSubTotal);
+    
+    
+}
+
+function display(parm1){
+    
+    document.getElementById("subtotal").value = parm1;
+    document.getElementById("total").value = parm1;
+    
+    enablebtnProceed();
+}
+
+function enablebtnProceed(){
+    $('#btnproceed').prop('disabled', false);
+}
+
+function disablebtnProceed() {
+    $('btnproceed').prop('disabled', true);
+}
